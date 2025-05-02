05e39d1773cca255ca799367c1dd9c28dcc735e0
Bgrerr <destructionlab@gmail.com>
2025-05-01 20:12:43 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
+const { sequelize } = require('./index');
 const bcrypt = require('bcrypt');
 
 // Definir el modelo de usuario
