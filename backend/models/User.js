d0e614d2d1e3463a7f525efa0f6e4a81e157af77
Bgrerr <destructionlab@gmail.com>
2025-05-01 20:22:45 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,37 +1,25 @@
+// backend/models/User.js
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
 const bcrypt = require('bcrypt');
 
-// Definir el modelo de usuario
-const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-});
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 
-// Encriptar contraseña antes de crear el usuario
-User.beforeCreate(async (user, options) => {
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-});
+  User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  });
 
-// ✅ Importar y definir la relación con Reservation
-const Reservation = require('./Reservation');
-
-User.hasMany(Reservation, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
-
-Reservation.belongsTo(User, {
-  foreignKey: 'userId'
-});
-
-module.exports = User;
+  return User;
+};
 
