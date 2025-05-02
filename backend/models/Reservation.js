776a2d27c312d4c435e4a44000b4c6b0311d63d1
Bgrerr <destructionlab@gmail.com>
2025-05-01 20:23:14 -0500

    Update Reservation.js

--- backend/models/Reservation.js
+++ backend/models/Reservation.js
@@ -1,28 +1,28 @@
+// backend/models/Reservation.js
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
 
-const Reservation = sequelize.define('Reservation', {
-  date: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  time: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  guests: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  notes: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  }
-});
-
-module.exports = Reservation;
+module.exports = (sequelize) => {
+  return sequelize.define('Reservation', {
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    guests: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  });
+};
 
